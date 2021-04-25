2a54d7628bf8451cf63ebb47f477260f95308eb8
DraganM2003 <mijatovic.dragan100@gmail.com>
2021-04-26 00:06:11 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define ("APPROOT" , dirname(dirname(__FILE__)));
+define ("URLROOT","http://localhost/shareposts/");
+define ("SITENAME", "SharePosts");
+define('APPVERSION', '1.0.0');
+
+define("DB_HOST" , "localhost");
+define("DB_USER" , "root");
+define("DB_PASS" , "K@c@0107");
+define("DB_NAME" , "shareposts");
+?>
